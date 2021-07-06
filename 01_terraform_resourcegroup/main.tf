91bce27c3b0664820f941954d77a0784697dafb8
atingupta2005 <atingupta2005@gmail.com>
2021-07-06 19:07:47 +0530

    Update main.tf

--- 01_terraform_resourcegroup/main.tf
+++ 01_terraform_resourcegroup/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "terraform-state-fab-rg"
+    storage_account_name  = "terraformstatefab056"
+    container_name        = "tstate"
+    key                   = "terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   version = ">=2.0"
   # The "feature" block is required for AzureRM provider 2.x.
